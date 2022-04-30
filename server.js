4f72ee9b73c228d912204e520a852a9f4471ca35
Avery Biskup <averybiskup@gmail.com>
2022-04-30 09:19:21 -0700

    work towards accessing youtube api

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-const express = require('express')
-const next = require('next')
+import express from 'express'
+import next from 'next'
+import { createRequire } from 'module'
+import axios from 'axios'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const require = createRequire(import.meta.url)
+const key = require('./key.json')
+const channel = '1Veritasium'
+
+const get_url = `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=${channel}&key=${key['api_key']}`
+
 app.prepare().then(() => {
   const server = express()
+
+  server.get('/get_vids', async (req, res) => {
+    axios.get(get_url)
+      .then((res) => {
+        console.log(res.data.items[0]['contentDetails']['relatedPlaylists']['uploads'])
+      })
+      .catch(err => {
+        console.log(err)  
+      })
+    res.send("HELLO")
+  })
 
   server.all('*', (req, res) => {
     return handle(req, res)
