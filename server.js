97747e2ddb64ebed60d91978830554edf66c2c80
Avery Biskup <averybiskup@gmail.com>
2022-04-30 18:18:04 -0700

    able to send video id of latest video to front end

--- server.js
+++ server.js
@@ -12,25 +12,55 @@ const require = createRequire(import.meta.url)
 const key = require('./key.json')
 
 const getPlaylistID = async (channel, key, callback) => {
-  const get_url = `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=${channel}&key=${key['api_key']}`
+  const getUrl = `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=${channel}&key=${key['api_key']}`
 
-  axios.get(get_url)
+  axios.get(getUrl)
     .then((res) => {
       callback(res.data.items[0]['contentDetails']['relatedPlaylists']['uploads'])
     })
     .catch(err => {
       console.log(err)  
+      callback(null)
     })
 }
 
+const getRecentVids = async (pid, key, callback) => {
+  const maxResults = 1
+  const playlistUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=${maxResults}&playlistId=${pid}&key=${key['api_key']}`
+  axios.get(playlistUrl)
+    .then((res) => {
+      const vid = res.data.items[0]['snippet']['resourceId']['videoId']
+      callback(vid)
+    })
+    .catch(err => {
+      console.log(err) 
+    })
+}
 
 app.prepare().then(() => {
   const server = express()
 
+  server.get('/getchannelpid', async (req, res) => {
+    const channel = '1Veritasium'
+    await getPlaylistID(channel, key, (channel_id) => {
+      if (channel_id) {
+        res.send(channel_id) 
+      } else {
+        res.send('Bad channel ID').status(501)  
+      }
+    })
+  })
+
   server.get('/getvids', async (req, res) => {
     const channel = '1Veritasium'
     await getPlaylistID(channel, key, (channel_id) => {
-      res.send(channel_id) 
+      if (channel_id) {
+        getRecentVids(channel_id, key, (vid) => {
+          res.send(vid)
+        })
+      } else {
+        res.send('Bad channel ID').status(501)  
+      }
     })
   })
 
