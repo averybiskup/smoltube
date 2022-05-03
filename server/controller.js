e5f5387afca642db99a6c67acd519ae2208112ba
Avery Biskup <averybiskup@gmail.com>
2022-05-03 08:45:32 -0700

    moved server handler code to controller.js

---
+++ server/controller.js
@@ -0,0 +1,84 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import axios from 'axios'
+import ChannelScheme from './models/channel.js'
+
+dotenv.config()
+
+const mongoDB = process.env.MONGO_URI
+mongoose.connect(mongoDB)
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const YOUTUBE_KEY = process.env.API_KEY
+
+const getPlaylistID = async (channel, key, callback) => {
+  const getUrl = `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=${channel}&key=${key}`
+
+  axios.get(getUrl)
+    .then((res) => {
+      const pid = res.data.items[0]['contentDetails']['relatedPlaylists']['uploads']
+      callback(pid)
+    })
+    .catch(err => {
+      console.log(err)  
+      callback(null)
+    })
+}
+
+const getRecentVids = async (pid, key, callback) => {
+  const maxResults = 1
+  const playlistUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=${maxResults}&playlistId=${pid}&key=${key}`
+  axios.get(playlistUrl)
+    .then((res) => {
+      const vid = res.data.items[0]['snippet']['resourceId']['videoId']
+      callback(vid)
+    })
+    .catch(err => {
+      console.log(err) 
+      callback(null)
+    })
+}
+
+export const getChannelPID = async (req, res) => {
+  const channel = '1Veritasium'  
+  await getPlaylistID(channel, YOUTUBE_KEY, (channel_id) => {
+    if (channel_id) {
+      res.send(channel_id)
+    } else {
+      res.send('Bad channel name').status(501)  
+    }
+  })
+}
+
+export const getVids = async (req, res) => {
+  const channel = '1Veritasium'
+  await getPlaylistID(channel, YOUTUBE_KEY, (channel_id) => {
+    if (channel_id) {
+      getRecentVids(channel_id, YOUTUBE_KEY, (vid) => {
+        res.send(vid)
+      })
+    } else {
+      res.send('Bad channel ID').status(501)  
+    }
+  })
+}
+
+export const postChannel = async (req, res) => {
+  const channelName = '1Veritasium'
+  const channelToInsert = new ChannelScheme({ name: channelName, 
+                                              pid: 'oI_X2cMHNe0', 
+                                              recentVideos: []})
+
+  await channelToInsert.save((err, data) => {
+    if (err) {
+      console.log('Error inserting document:', err, data)
+      res.status(501).send('Failed to insert document')
+    } else {
+      console.log('Added document')
+      res.status(200).send('Added document')
+    }
+  })
+}
--- server/server.js
+++ server/server.js
@@ -1,100 +1,21 @@
 import express from 'express'
 import next from 'next'
-import { createRequire } from 'module'
-import axios from 'axios'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import ChannelScheme from './models/channel.js'
+import { getChannelPID, 
+         getVids, 
+         postChannel } from './controller.js'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-dotenv.config()
-
-// youtube api key
-const key = process.env.API_KEY
-
-const getPlaylistID = async (channel, key, callback) => {
-  const getUrl = `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=${channel}&key=${key}`
-
-  axios.get(getUrl)
-    .then((res) => {
-      const pid = res.data.items[0]['contentDetails']['relatedPlaylists']['uploads']
-      callback(pid)
-    })
-    .catch(err => {
-      console.log(err)  
-      callback(null)
-    })
-}
-
-const getRecentVids = async (pid, key, callback) => {
-  const maxResults = 1
-  const playlistUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=${maxResults}&playlistId=${pid}&key=${key}`
-  axios.get(playlistUrl)
-    .then((res) => {
-      const vid = res.data.items[0]['snippet']['resourceId']['videoId']
-      callback(vid)
-    })
-    .catch(err => {
-      console.log(err) 
-      callback(null)
-    })
-}
-
 app.prepare().then(() => {
   const server = express()
 
-  const mongoDB = process.env.MONGO_URI
-  mongoose.connect(mongoDB)
+  server.get('/getchannelpid', getChannelPID)
+  server.get('/getvids',       getVids)
 
-  const db = mongoose.connection
-
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-  server.get('/getchannelpid', async (req, res) => {
-    const channel = '1Veritasium'
-    await getPlaylistID(channel, key, (channel_id) => {
-      if (channel_id) {
-        res.send(channel_id)
-      } else {
-        res.send('Bad channel name').status(501)  
-      }
-    })
-  })
-
-  server.get('/getvids', async (req, res) => {
-    const channel = '1Veritasium'
-    await getPlaylistID(channel, key, (channel_id) => {
-      if (channel_id) {
-        getRecentVids(channel_id, key, (vid) => {
-          res.send(vid)
-        })
-      } else {
-        res.send('Bad channel ID').status(501)  
-      }
-    })
-  })
-
-  server.post('/postchannels', async (req, res) => {
-    const channelName = '1Veritasium'
-    const channelToInsert = new ChannelScheme({ name: channelName, 
-                                                pid: 'oI_X2cMHNe0', 
-                                                recentVideos: []})
-
-    await channelToInsert.save((err, data) => {
-      if (err) {
-        console.log('Error inserting document:', err, data)
-        res.status(501).send('Failed to insert document')
-      } else {
-        console.log('Added document')
-        res.status(200).send('Added document')
-      }
-    })
-
-  })
+  server.post('/postchannels', postChannel)
 
   server.all('*', (req, res) => {
     return handle(req, res)
